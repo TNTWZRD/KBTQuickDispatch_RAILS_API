73bca2c24230e14a145a8b014b13063881a8dad2
Daniel Jajliardo <TNTWZRD.minecraft@gmail.com>
2025-06-13 01:39:03 -0400

    Update Dockerfile to use default MySQL client and change exposed port to 3000. Modify CORS initializer to allow requests from any origin on port 5173.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5173" # Replace with your frontend domain
+    origins "*:5173" # Replace with your frontend domain
     
     resource "*",
       headers: :any,
