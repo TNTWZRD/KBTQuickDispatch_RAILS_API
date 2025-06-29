1e72411c6da838077c1903f0cef89adf17d7b0cf
Daniel Jajliardo <TNTWZRD.minecraft@gmail.com>
2025-06-28 22:19:14 -0400

    test

--- app/controllers/api/v1/drivers_controller.rb
+++ app/controllers/api/v1/drivers_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::DriversController < Api::V1::BaseController
   def getDrivers
     drivers = User.all
     drivers.each do |driver|
-      drivers.delete(driver) unless driver.is_driver()
+      drivers.delete(driver) unless driver.is_driver?
     end
     render json: drivers, status: :ok
   end
