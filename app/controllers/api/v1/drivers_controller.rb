101128dfc6a71f8d5509385b295601cb5d427b71
Daniel Jajliardo <TNTWZRD.minecraft@gmail.com>
2025-06-28 22:12:38 -0400

    Remove unnecessary respond_to declaration in getDrivers method

--- app/controllers/api/v1/drivers_controller.rb
+++ app/controllers/api/v1/drivers_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::DriversController < Api::V1::BaseController
   before_action :authenticate_user!
 
   def getDrivers
-    respond_to :json
     drivers = User.where("role & ? = ?", User.roles[:driver], User.roles[:driver])
     render json: drivers, status: :ok
   end
