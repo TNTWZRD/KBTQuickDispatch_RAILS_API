3f43d4dd39294534687f02e4e43690e5daf36b24
Daniel Jajliardo <TNTWZRD.minecraft@gmail.com>
2025-06-28 22:18:47 -0400

    Refactor getDrivers method to filter drivers using is_driver check

--- app/controllers/api/v1/drivers_controller.rb
+++ app/controllers/api/v1/drivers_controller.rb
@@ -3,7 +3,10 @@ class Api::V1::DriversController < Api::V1::BaseController
   before_action :authenticate_user!
 
   def getDrivers
-    drivers = User.where("role & ? = ?", User.ROLE_VAL[:driver], User.ROLE_VAL[:driver])
+    drivers = User.all
+    drivers.each do |driver|
+      drivers.delete(driver) unless driver.is_driver()
+    end
     render json: drivers, status: :ok
   end
 
