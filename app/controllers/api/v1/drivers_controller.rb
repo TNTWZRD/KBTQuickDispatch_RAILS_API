fe4bb13b939553b63b42d2fdfcbed00830ecb453
Daniel Jajliardo <TNTWZRD.minecraft@gmail.com>
2025-07-02 20:29:02 -0400

    Fix driver name assignment in getDrivers method

--- app/controllers/api/v1/drivers_controller.rb
+++ app/controllers/api/v1/drivers_controller.rb
@@ -6,8 +6,12 @@ class Api::V1::DriversController < Api::V1::BaseController
 
     render json: {status: "Unauthorized"}, status: :unauthorized unless current_user.is_dispatcher? || current_user.is_manager? || current_user.is_owner? || current_user.is_admin? 
 
-    drivers = Driver.all.order(:name).select(:id, :name, :phone_number, :user_id, :status)
+    drivers = Driver.all.order(:name).select(:id, :name, :phone_number, :user_id, :status, :emergency_contact_names, :emergency_contact_numbers)
     # drivers = User.where('role & ? = ?', 1, 1)
+    drivers = drivers.map do |driver|
+      driver.name = driver.user.name if driver.user.present? && driver.name.blank?
+      driver
+    end
     render json: drivers, status: :ok
   end
 
